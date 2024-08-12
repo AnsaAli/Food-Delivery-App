63451d3be56732f0a5875c3c6d994a9f05a1880d
AnsaAli <chinjuansa@example.com>
2024-08-12 11:43:34 +0530

    Defined a customHook to fetch the restau data in RestaurantMenu component

--- src/component/RestaurantMenu.js
+++ src/component/RestaurantMenu.js
@@ -1,23 +1,26 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { Menu_Api } from "../utility/constants";
-
+// import { Menu_Api } from "../utility/constants";
+import useRestaurantMenu from "../utility/useRestaurantMenu";
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+  //   const [resInfo, setResInfo] = useState(null);
 
   const { resId } = useParams();
+  const resInfo = useRestaurantMenu(resId); //creating a custom hook, for making RestaurantMenu component's job only for displaying the resaturants.
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
+  //defined a custom hook to fetch the restaurant datas
+  //   useEffect(() => {
+  //     fetchMenu();
+  //   }, []);
 
-  const fetchMenu = async () => {
-    const data = await fetch(Menu_Api + resId);
-    const json = await data.json();
+  //   const fetchMenu = async () => {
+  //     const data = await fetch(Menu_Api + resId);
+  //     const json = await data.json();
 
-    setResInfo(json.data);
-  };
+  //     setResInfo(json.data);
+  //   };
+
   if (resInfo === null) return <Shimmer />;
 
   const { name, costForTwoMessage, cuisines } = resInfo.cards[2].card.card.info;
---
+++ src/utility/useRestaurantMenu.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { Menu_Api } from "../utility/constants";
+
+const useRestaurantMenu = (resId) => {
+    const [resInfo, setResInfo] = useState(null)
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+  //fetching res data
+  const fetchMenu = async ()=>{
+    const data = await fetch(Menu_Api + resId) ;
+    const json = await data.json();
+    setResInfo(json.data);
+  }
+
+  return resInfo;
+};
+export default useRestaurantMenu;
